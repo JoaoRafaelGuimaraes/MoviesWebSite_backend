5dc4520223197d41a9696d42f55fabebbc2ac7f2
luccacb16 <lucca.barberato@gmail.com>
2023-10-18 21:29:22 -0300

    Removendo src/controllers/teste.ts

--- src/controllers/teste.ts
+++
@@ -1,6 +0,0 @@
-import { getSimilarMoviesById } from "./filmeControllers";
-import { getSimilarSeriesById } from "./serieControllers";
-
-getSimilarSeriesById(48891).then((series) => {
-    console.log(series);
-});
