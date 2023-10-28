517c51ef75f0633ace209c44355b3e79c8dd7895
joao.guimaraes590427 <joao.guimaraes@catijr.com.br>
2023-10-28 17:16:12 -0300

    Adicionei o plugin fastify-cors para rodar servidor em duas portas diferentes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import Fastify from 'fastify';
+const fastify = require('fastify')({ logger: true });
+import cors from '@fastify/cors';
 import userAuthRoutes from './routes/userAuthRoutes';
 import userInfoRoutes from './routes/userInfoRoutes';
 import userFavoritesRoutes from './routes/userFavoritesRoutes';
 import titulosRoutes from './routes/titulosRoutes';
 
+
+
+
 export const server = Fastify({
     logger: false,
 });
+
+server.register(cors, {
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST", "OPTIONS"]
+});
+
 
 // Rotas
 userAuthRoutes(server);
